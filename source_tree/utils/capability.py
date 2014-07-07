7a6f6316f27d71f72cf2b1165cf09bf9d9a581e8
Ilya Korovin <ilya.krvn@gmail.com>
2014-07-07 15:12:14 +0400

    fixed bug with capability

--- source_tree/utils/capability.py
+++ source_tree/utils/capability.py
@@ -11,8 +11,6 @@ def check_capability_ex(request, user_type):
 
 
 def check_capability_course(request, user_type):
-    if user_type == "teacher":
-        return
     sec_key = request.params['sec_key'] if 'sec_key' in request.params else ''
     if sec_key != request.registry.settings['source_tree.course.sec_key'] \
             and not RequestCheckUserCapability(request, 'moodle/course_tree:' + user_type):
