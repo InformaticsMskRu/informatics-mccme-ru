afcf28166c899a40ff9bf7d4157bf7d229245bdf
Maxim Pestryakov <maximpestryakov@gmail.com>
2017-12-13 01:51:16 +0300

    Fixed get_group return value

--- pynformatics/view/group.py
+++ pynformatics/view/group.py
@@ -4,6 +4,7 @@ from pyramid.view import view_config
 
 from pynformatics.model.group import Group
 from pynformatics.model.group import UserGroup
+from pynformatics.model.user import User
 from pynformatics.models import DBSession
 from pynformatics.utils.context import with_context
 
@@ -20,15 +21,18 @@ def get_group(request, context):
                 "message": "group with id = {} not found".format(group_id)
             }
 
-        group = group.serialize()
+        owner = DBSession.query(User).filter(User.id == group.owner_id).first().serialize(context)
         users = DBSession.query(UserGroup).filter(UserGroup.group_id == group_id).all()
         for i in range(len(users)):
             users[i] = users[i].user.serialize(context)
 
-        return {
-            "group": group,
-            "users": users
-        }
+        group = group.serialize()
+        del group['owner_id']
+        group.update({
+            'owner': owner,
+            'users': users
+        })
+        return group
 
     except Exception as e:
         return {
