222081977c7ef6c2a475d10a894d4ba0aa57dd57
root <root@new.informatics.msk.ru>
2018-02-07 00:51:27 +0300

    run_id

--- pynformatics/contest/ejudge/ejudge_proxy.py
+++ pynformatics/contest/ejudge/ejudge_proxy.py
@@ -6,40 +6,30 @@ from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email.utils import COMMASPACE, formatdate
 from email import encoders
+import json
 
 default_error_str = 'Ошибка отправки задачи'
 
 status_repr = {
   0 : 'Задача отправлена на проверку', # NEW_SRV_ERR_NO_ERROR
-119 : 'Отправка пустого файла',        # NEW_SRV_ERR_FILE_EMPTY
-104 : 'Отправка бинарного файла',      # NEW_SRV_ERR_BINARY_FILE
- 81 : 'Эта посылка является копией предыдущей',# NEW_SRV_ERR_DUPLICATE_SUBMIT
- 36 : 'Этот язык не может быть использован для этой задачи. Обратитесь к администраторам.',
- 82 : 'Задача уже решена',             # NEW_SRV_ERR_PROB_ALREADY_SOLVED
- 77 : 'Отправляемый файл превышает допустимый размер (64K) или превышена квота на число посылок (обратитесь к админимтратору)', # NEW_SRV_ERR_RUN_QUOTA_EXCEEDED
-112 : 'Отправляемый файл пустой', # SUBMIT_EMPTY 
+120 : 'Отправка пустого файла',        # NEW_SRV_ERR_FILE_EMPTY
+105 : 'Отправка бинарного файла',      # NEW_SRV_ERR_BINARY_FILE
+ 82 : 'Эта посылка является копией предыдущей',# NEW_SRV_ERR_DUPLICATE_SUBMIT
+ 37 : 'Этот язык не может быть использован для этой задачи. Обратитесь к администраторам.',
+ 83 : 'Задача уже решена',             # NEW_SRV_ERR_PROB_ALREADY_SOLVED
+ 78 : 'Отправляемый файл превышает допустимый размер (64K) или превышена квота на число посылок (обратитесь к админимтратору)', # NEW_SRV_ERR_RUN_QUOTA_EXCEEDED
+113 : 'Отправляемый файл пустой', # SUBMIT_EMPTY 
 1000: 'Отправляемый файл превышает допустимый размер. Требуется отправить исходный код или текстовый файл',
 }
 
 import codecs
 
 def report_error(code, login_data, submit_data, file, filename, user_id, addon = ''):
-    msg = MIMEMultipart()
-    msg['From'] = 'ejudge.submitter'
-    msg['To'] = 'andrey.shestimerov@gmail.com'
-    msg['Date'] = formatdate(localtime=True)
-    subject = 'ejudge_submit from ' + user_id
-    if (code):
-        subject = subject + ' code ' + code.group(1)
-    msg['Subject'] = subject
     t = str({'info' : addon, 'login_data' : login_data, 'submit_data' : submit_data, 'filename' : filename})
-    msg.attach(MIMEText(t))
-    log=codecs.open('/tmp/python.log', 'a', 'utf-8')
+    log=codecs.open('/var/log/python.log', 'a', 'utf-8')
     log.write(t)
+    log.write('\n---\n')
     log.close()
-    smtp = smtplib.SMTP('localhost')
-#    smtp.sendmail('ejudge.submitter', 'vrandik@gmail.com', msg.as_string())
-    smtp.close()
 
 def submit(run_file, contest_id, prob_id, lang_id, login, password, filename, url, user_id):
     login_data = {
@@ -51,9 +41,6 @@ def submit(run_file, contest_id, prob_id, lang_id, login, password, filename, ur
     }
 
     c = requests.post(url, data = login_data)
-
-#    return c.text
-
     res = re.search('SID="([^"]*)";', c.text)
 
     if (res):
@@ -63,31 +50,28 @@ def submit(run_file, contest_id, prob_id, lang_id, login, password, filename, ur
         return default_error_str
 
     cookies = c.cookies
-    files = {'file' : (filename, run_file) }
+    files = {'file' : (filename, run_file)}
 
     submit_data = {
         'SID' : SID,
         'prob_id' : prob_id,
         'lang_id' : lang_id,
-        'action_40' : 'action_40'
+        'action_40' : 'action_40',
+        'json' : 1,
     }
 
     c = requests.post(url, data = submit_data, cookies = cookies, files = files)
 
-    if "method=\"post\"" in c.text:
+    resp = json.loads(c.text)
+
+    if "run_id" in resp:
         return "ok"
 
-    ret_code = re.search('code\'\s\:\s(\d*)', c.text)
-
-    if (ret_code):
-        code = int(ret_code.group(1))
-        if code in status_repr:
-            return status_repr[code]
-        else:
-            report_error(ret_code, login_data, submit_data, run_file, filename, user_id, c.text)
-            return default_error_str
+    code = resp["error_code"]
+    if code in status_repr:
+        return status_repr[code]
     else:
-        report_error(None, login_data, submit_data, run_file, filename, user_id, c.text)
+        report_error(code, login_data, submit_data, run_file, filename, user_id, c.text)
         return default_error_str
 
 
