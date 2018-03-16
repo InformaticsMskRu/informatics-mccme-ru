7b09adfb0e447ad7339c3f2171c0181e65c9dc12
Maxim Grishkin <grishkin.max@gmail.com>
2018-03-16 09:50:42 +0300

    Логирование размера очереди сабмитов

--- pynformatics/contest/ejudge/submit_queue/queue.py
+++ pynformatics/contest/ejudge/submit_queue/queue.py
@@ -1,18 +1,27 @@
+import logging
 from gevent.queue import Queue
 
 from .submit import Submit
 from .worker import SubmitWorker
 
 
+log = logging.getLogger('SubmitQueue')
+
+
 class SubmitQueue(Queue):
     def __init__(self, workers):
         super(SubmitQueue, self).__init__()
+        self.total_in = 0
         self.workers = [
             SubmitWorker(self)
             for _ in range(workers)
         ]
         for worker in self.workers:
             worker.start()
+
+    @property
+    def total_out(self):
+        return sum([worker.submitted for worker in self.workers])
 
     def submit(self, context, file, language_id, ejudge_url):
         self.put(
@@ -23,3 +32,5 @@ class SubmitQueue(Queue):
                 ejudge_url=ejudge_url,
             )
         )
+        self.total_in += 1
+        log.info('Current size: %s. Total submitted: %s.', self.total_in - self.total_out, self.total_out)
--- pynformatics/contest/ejudge/submit_queue/worker.py
+++ pynformatics/contest/ejudge/submit_queue/worker.py
@@ -12,15 +12,17 @@ class SubmitWorker(Greenlet):
     def __init__(self, queue):
         super(SubmitWorker, self).__init__()
         self.queue = queue
+        self.submitted = 0
 
     def _run(self):
         while True:
             if not self.queue:
                 sleep(1)
                 continue
-
             try:
                 submit = self.queue.get()
                 submit.send()
             except Exception:
                 log.exception('Submit worker caught exception and skipped submit without notifying user')
+
+            self.submitted += 1
