4d186b6926e0cd634badae6bf00ebeece2a4a35a
n.pakhtusov <n.pakhtusov@tinkoff.ru>
2018-12-13 17:50:12 +0300

    feat: Submit function from production

--- pynformatics/view/problem.py
+++ pynformatics/view/problem.py
@@ -37,36 +37,34 @@ def problem_show_limits(request):
         return {"result" : "error", "message" : e.__str__(), "stack" : traceback.format_exc()}
 
 
-@view_config(route_name='problem.submit', renderer='json',  request_method='POST')
+@view_config(route_name='problem.submit', renderer='json')
 def problem_submits(request):
     user_id = RequestGetUserId(request)
+    user = DBSession.query(SimpleUser).filter(SimpleUser.id == user_id).first()
     lang_id = request.params["lang_id"]
     problem_id = request.matchdict["problem_id"]
+    problem = DBSession.query(EjudgeProblem).filter(EjudgeProblem.id == problem_id).first()
     input_file = request.POST['file'].file
+    filename = request.POST['file'].filename
+    ejudge_url = request.registry.settings['ejudge.new_client_url']
 
-    _data = {
-        'lang_id': lang_id,
-        'user_id': user_id,
-    }
-    _prob_id = problem_id
-    url = 'http://localhost:12346/problem/trusted/{}/submit_v2'.format(_prob_id)
+    res = submit(input_file, problem.ejudge_contest_id, problem.problem_id, lang_id, user.login, user.password, filename, ejudge_url, user_id)
     try:
+        input_file.seek(0)
+        print('#######\nin try')
+        _data = {
+            'lang_id': lang_id,
+            'user_id': user_id,
+        }
+        _prob_id = problem_id
+        url = 'http://localhost:12346/problem/trusted/{}/submit_v2'.format(_prob_id)
+        import requests
         _resp = requests.post(url, files={'file': input_file}, data=_data)
-    except requests.exceptions.RequestException:
-        raise exc.HTTPInternalServerError('Can not send submit to submit queue')
-    if _resp.status_code != 200:
-        raise exc.HTTPInternalServerError('Something went wrong while submit sending')  # Поведение не изменилось
+        print('Response from :12346', _resp)
+    except Exception as e:
+        print(e)
 
-    # TODO: Надо провести все проверки, которые делает ej на отправляемый файл
-    # TODO: ещё до тестирования, и вернуть нужный код ошибки
-    code = 0  # Задача отправлена на проверку
-    return {
-        'res':
-            {
-                'code': code,
-                'message': status_repr[0]
-            }
-    }
+    return {'res': res}
 
 
 @view_config(route_name='problem.ant.submit', renderer='json')
