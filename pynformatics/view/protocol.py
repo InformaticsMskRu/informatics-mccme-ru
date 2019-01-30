47176eae4b8b698ce405308b24d84f708c2544ac
n.pakhtusov <n.pakhtusov@tinkoff.ru>
2019-01-30 19:26:20 +0300

    Fix (NFRMTCS-49): Update view for downloading submit archive

--- pynformatics/view/protocol.py
+++ pynformatics/view/protocol.py
@@ -2,9 +2,10 @@ import zipfile
 from io import BytesIO
 
 import requests
-from phpserialize import *
+from pynformatics import EjudgeProblem
 from pyramid.response import Response
 from pyramid.view import view_config
+from pynformatics.models import DBSession
 
 from pynformatics.model import Run
 from pynformatics.model.run import get_lang_ext_by_id
@@ -129,32 +130,52 @@ def get_submit_archive(request):
     recaptha_resp = request.params['g-recaptcha-response']
     if not check_captcha(recaptha_resp, request.registry.settings["recaptcha.secret"]):
         return "Не получилось"
-    contest_id = int(request.matchdict['contest_id'])
-    run_id = int(request.matchdict['run_id'])
-    sources = "sources" in request.params
-    all_tests = "all_tests" in request.params
-    tests = request.params.get("tests", "")
-    tests_set = set()
-    for i in tests.split(" "):
-        try:
-            tests_set.add(int(i))
-        except ValueError:
-            pass
 
-    run = Run.get_by(run_id=run_id, contest_id=contest_id)
-    run.parsetests
-    prob = run.problem
+    run_id = int(request.matchdict['run_id'])
+    problem_id = int(request.params['problem_id'])
+    need_all_tests = "all_tests" in request.params
+    need_test_numbers = request.params.get("tests", "")
+    if not need_all_tests and need_test_numbers:
+        need_test_numbers = need_test_numbers.split(' ')
+        tests_set = set(map(int, need_test_numbers))
+    else:
+        tests_set = set()
+
     archive = BytesIO()
     zf = zipfile.ZipFile(archive, "w", zipfile.ZIP_DEFLATED)
 
-    run.tested_protocol
-    for i in range(1, run.tests_count + 1):
-        if all_tests or i in tests_set:
-            zf.writestr("tests/{0:02}".format(i), prob.get_test(i, prob.get_test_size(i)))
-            zf.writestr("tests/{0:02}.a".format(i), prob.get_corr(i, prob.get_corr_size(i)))
+    # Download source and info about run
+    params = {
+        'is_admin': True,
+    }
+    try:
+        url = 'http://localhost:12346/problem/run/{}/source/'.format(run_id)
+        resp = requests.get(url, params=params)
+        content = resp.json()
+        data = content['data']
+        lang_id = data['language_id']
+        source = data['source']
+    except (requests.RequestException, ValueError) as e:
+        print('Request to :12346 failed!')
+        print(str(e))
+        return {"result": "error", "message": str(e), "stack": traceback.format_exc()}
 
-    if sources:
-        zf.writestr("{0}{1}".format(run_id, get_lang_ext_by_id(run.lang_id)), run.get_sources())
+    source_name = "{0}{1}".format(run_id, get_lang_ext_by_id(lang_id))
+    zf.writestr(source_name, source)
+
+    # TODO: Перенести логику выкачивания тестов проблем в rmatics/ejudge-core
+    prob = DBSession.query(EjudgeProblem).get(problem_id)
+
+    # TODO: Здесть не 100, а некое максимальное число тестов
+    # TODO: Изначально здесь был EjudgeRun.tests_count, который брался из xml
+    # TODO: Но мы его пофиксили
+    for i in range(1, 100):
+        if need_all_tests or i in tests_set:
+            try:
+                zf.writestr("tests/{0:02}".format(i), prob.get_test(i, prob.get_test_size(i)))
+                zf.writestr("tests/{0:02}.a".format(i), prob.get_corr(i, prob.get_corr_size(i)))
+            except FileNotFoundError:
+                break
 
     checker_src, checker_ext = prob.get_checker()
     zf.writestr("checker{}".format(checker_ext), checker_src)
@@ -162,6 +183,8 @@ def get_submit_archive(request):
     zf.close()
     archive.seek(0)
     response = Response(content_type="application/zip",
-                        content_disposition='attachment; filename="archive_{0}_{1}.zip"'
-                        .format(contest_id, run_id), body=archive.read())
+                        content_disposition='attachment; filename="archive_{0}_{1}.zip"'.format(
+                            problem_id, run_id),
+                        body=archive.read())
     return response
+
